659f8f6c9c1c1ec2fa0004b64321f7e1f961dd84
Denis Kudakov <denn88kudakov@yandex.ru>
2022-08-03 20:50:30 +0300

    Выполнил все задания проекта

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import filters, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
-from posts.models import Follow, Group, Post, User
+from posts.models import Group, Post, User
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
                           PostSerializer)
